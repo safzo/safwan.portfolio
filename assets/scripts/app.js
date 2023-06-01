5fdfdfe7f58c3602742ba2908903ed9cab07b121
Safwan Hasan <43386254+safzo@users.noreply.github.com>
2023-05-31 20:24:00 -0500

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -54,7 +54,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love everything about coding.", "also play competitive golf.", "solve problems."
+				"am a Data Scientist.", "try to optimize capacity and efficiency.", "also play competitive golf.", "also love Formula 1, and Verstappen.", "solve problems."
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
